d80fad92c020b13de212e8488a8471d27abd034e
Sorabh Tomar <me@sorabhtomar.com>
2022-06-24 04:04:01 +0530

    Migrated template engine from Freemarker to Thymeleaf

---
+++ Code Sharing Platform/task2/src/platform/presentation/web/CodeController.java
@@ -0,0 +1,45 @@
+package platform.presentation.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+import platform.business.entities.Code;
+import platform.business.service.CodeService;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller("/code")
+public class CodeController {
+    @Autowired
+    private CodeService codeService;
+
+    @GetMapping(value = "/code/{id}", produces = "text/html")
+    public String getCodeSnippet(@PathVariable String id, Model model) {
+        Optional<Code> optionalCode = codeService.getCodeById(id);
+
+        if (optionalCode.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        model.addAttribute("title", "Code");
+        model.addAttribute("codes", optionalCode.get());
+        return "codes";
+    }
+
+    @GetMapping(value = "/code/new", produces = "text/html")
+    public String newCodeSnippet() {
+        return "create";
+    }
+
+    @GetMapping(value = "/code/latest", produces = "text/html")
+    public String getLatestCodeSnippets(Model model) {
+        model.addAttribute("title", "Latest");
+        model.addAttribute("codes", codeService.getLatestCodes());
+        return "codes";
+    }
+}
